372e532d6a242dc3a422add387512d1302118dc5
Eric Hubbell <eric@erichubbell.com>
2024-07-09 12:17:14 -0600

    Remove classnames

--- src/tailwind/tailwind-functions.tsx
+++ src/tailwind/tailwind-functions.tsx
@@ -1,7 +1,29 @@
-import classNames from 'classnames/bind';
+import { isEmpty, isObject } from 'utils';
+
+export const computeProps = value => {
+	return isObject(value) ? value : {};
+};
+
+export const formatProps = (props, name?) => {
+	const formattedProps = props;
+	const active = props.active || {};
+	Object.keys(active).map(key => (formattedProps[key] = active[key]));
+	return formattedProps;
+};
 
 export const tailwindClassBuilder = (props, name?) => {
-	return classNames.bind(props);
+	let classes = '';
+	const formattedProps = formatProps(props, name);
+	Object.keys(formattedProps)
+		.filter((v, i, s) => s.indexOf(v) === i)
+		.filter(v => v !== 'type')
+		.map(key => {
+			const value = formattedProps[key];
+			if (isObject(value)) return;
+			if (isEmpty(value)) return;
+			classes = classes.concat(value + ' ');
+		});
+	return classes;
 };
 
 // Docs
