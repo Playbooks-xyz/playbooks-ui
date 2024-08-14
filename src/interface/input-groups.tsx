ea61a1282167627163bcb9bcd66b327fac0c41cc
Eric Hubbell <eric@erichubbell.com>
2024-08-14 12:38:22 -0600

    Update input-groups

--- src/interface/input-groups.tsx
+++ src/interface/input-groups.tsx
@@ -8,8 +8,7 @@ export const InputGroup = ({ id, name = 'InputGroup', active, className, childre
 	const base = {
 		bgColor: 'bg-gray-white dark:bg-gray-900',
 		border: 'border',
-		borderColor:
-			'border-gray-400 dark:border-gray-500 focus-within:border-blue-500 dark:focus-within:border-cyan-500',
+		borderColor: 'border-gray-400 dark:border-gray-500 focus-within:border-blue-500 dark:focus-within:border-cyan-500',
 		borderOpacity: 'border-opacity-50 dark:border-opacity-50',
 		borderRadius: 'rounded-md',
 		color: 'gray-600 dark:gray-200',
@@ -17,7 +16,7 @@ export const InputGroup = ({ id, name = 'InputGroup', active, className, childre
 		ring: 'focus-within:ring-1',
 		ringColor: 'focus-within:ring-transparent dark:focus-within:ring-transparent',
 		ringOffset: 'focus-within:ring-offset-1',
-		ringOffsetColor: 'focus-within:ring-offset-blue-500 focus-within:dark:ring-offset-orange-500',
+		ringOffsetColor: 'focus-within:ring-offset-blue-500 focus-within:dark:ring-offset-cyan-500',
 		transition: 'transition ease',
 		// width: 'w-full',
 	};
