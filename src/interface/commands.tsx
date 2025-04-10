e5dbba5a39d61309e719f0e39c67f500098308f3
playbooks <eric@erichubbell.com>
2025-04-10 10:51:44 -0600

    command interface

--- src/interface/commands.tsx
+++ src/interface/commands.tsx
@@ -2,29 +2,16 @@ import { useMemo } from 'react';
 
 import { iCommand } from 'interface/commands.types';
 import { Span } from 'interface/html';
-import keycode from 'keycode';
-import { borderProps, computeBadgeSize } from 'utils';
+import { borderProps } from 'utils';
 
-export const Command = ({
-	id,
-	name = 'CMD',
-	size = 'sm',
-	variant = '',
-	keys = [],
-	className,
-	children,
-	...tailwind
-}: iCommand) => {
+export const Command = ({ id, name = 'CMD', keys = [], className, children, ...tailwind }: iCommand) => {
 	const base = {
-		size: computeBadgeSize(size),
 		...borderProps,
-		borderRadius: 'rounded-md',
-		color: 'text-gray-600 dark:text-gray-200',
+		color: 'text-gray-500 dark:text-gray-400',
 		flex: 'shrink-0',
-		fontFamily: 'font-accent',
+		fontFamily: 'font-secondary',
 		fontSize: 'text-xs',
 		fontWeight: 'font-medium',
-		space: 'space-x-2',
 		textTransform: 'capitalize',
 		whitespace: 'whitespace-nowrap',
 	};
@@ -32,10 +19,27 @@ export const Command = ({
 
 	// Computed
 	const computedKeys = useMemo(() => {
-		return keys.map(code => {
-			const keyCode = keycode.codes[code];
-			return String.fromCharCode(keyCode);
+		const formattedKeys = [];
+		keys.map(code => {
+			switch (code) {
+				case 'command':
+				case 'Command':
+					return formattedKeys.push('⌘');
+				case 'control':
+				case 'Control':
+					return formattedKeys.push('^');
+				case 'enter':
+				case 'Enter':
+					return formattedKeys.push('↵');
+				case 'option':
+				case 'Option':
+					return formattedKeys.push('⌥');
+				case 'shift':
+				case 'Shift':
+					return formattedKeys.push('⇧');
+			}
 		});
+		return formattedKeys;
 	}, [keys]);
 
 	// Render
--- src/interface/commands.types.tsx
+++ src/interface/commands.types.tsx
@@ -1,7 +1,6 @@
 import { HtmlType } from 'types';
 
 export type iCommand = HtmlType & {
-	size?: string;
 	keys?: string[];
 	variant?: string;
 };
