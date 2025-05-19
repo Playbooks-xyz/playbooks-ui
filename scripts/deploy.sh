5e53d48682f9d191af50a0b6ceb2dd69aa17180d
playbooks <eric@erichubbell.com>
2025-05-19 11:38:27 -0600

    script updates

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-  echo -e " \n please include a commit message. \n"
+  echo -e " \nplease include a commit message.\m"
   exit
 fi
 
-echo -e "\n commiting changes... \n"
+if [ -z "$2" ]; then
+  echo -e " \nplease include a version.\m"
+  exit
+fi
+
+echo -e "\n deploying updates...\n"
 
 echo -e "\n git checkout main \n"
 git checkout main & checkout_id=$!
@@ -25,6 +30,29 @@ if [ $? -eq 1 ]; then exit; fi
 echo -e "\n git commit \n"
 git commit -m "$1" & commit_pid=$!
 wait $commit_pid
+if [ $? -eq 1 ]; then exit; fi
+
+echo -e "\n git push \n"
+git push & push_id=$!
+wait $push_id
+if [ $? -eq 1 ]; then exit; fi
+
+echo -e "\n npm version \n"
+npm version $2
+
+echo -e "\n npm build \n"
+npm run build & build_id=$!
+wait $build_id
+if [ $? -eq 1 ]; then exit; fi
+
+echo -e "\n npm publish \n"
+npm publish & publish_id=$!
+wait $publish_id
+if [ $? -eq 1 ]; then exit; fi
+
+echo -e "\n git push tags \n"
+git push --tags & push_id=$!
+wait $push_id
 if [ $? -eq 1 ]; then exit; fi
 
 echo -e "\n git push \n"
