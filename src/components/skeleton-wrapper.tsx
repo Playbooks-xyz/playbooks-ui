0c81abe41e4dd22ffb5e0050fd6f18bd913a1375
playbooks <eric@erichubbell.com>
2025-06-17 10:38:49 -0600

    skeleton updates

--- src/components/skeleton-wrapper.tsx
+++ src/components/skeleton-wrapper.tsx
@@ -4,19 +4,17 @@ import { TailwindProps } from '@ehubbell/html';
 import { computeTailwind } from 'utils';
 
 export type SkeletonProps = {
-	height?: string;
-	width?: string;
 	tailwind?: TailwindProps;
 	className?: string;
 };
 
-const Skeleton = ({ height = '100%', width = '100%', tailwind, className = '', ...props }: SkeletonProps) => {
-	const base = { width: 'w-full' };
+const Skeleton = ({ tailwind, className = '', ...props }: SkeletonProps) => {
+	const base = { height: 'h-full', width: 'w-full' };
 	const classes = computeTailwind({ ...base, ...tailwind, ...props, className });
 
 	return (
 		<SkeletonTheme>
-			<SkeletonWrapper height={height} width={width} className={classes} />
+			<SkeletonWrapper className={classes} />
 		</SkeletonTheme>
 	);
 };
